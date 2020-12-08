aaf8ac4bc83d93be678b06483502cf86b0229f29
danielsauve <danielsauve@protonmail.com>
2020-12-07 21:13:36 -0800

    Solution to Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -17,20 +17,36 @@ def parse_password_rules(password_rule: str) -> PasswordRules:
     return PasswordRules(int(low), int(high), letter[0])
 
 
-def validate_password(password_rule: PasswordRules, password: str) -> bool:
+def validate_password_old(password_rule: PasswordRules, password: str) -> bool:
     count = password.count(password_rule.letter)
     return password_rule.min_count <= count <= password_rule.max_count
 
 
-def part1(password_rules_list: List[str]) -> int:
+def part_1(password_rules_list: List[str]) -> int:
     valid_passwords = 0
     for item in password_rules_list:
         rules = parse_password_rules(item)
         password = item.split()[2]
-        valid_passwords += 1 if validate_password(rules, password) else 0
+        valid_passwords += 1 if validate_password_old(rules, password) else 0
+    return valid_passwords
+
+
+def validate_password_new(password_rule: PasswordRules, password: str) -> bool:
+    first = password[password_rule.min_count - 1]
+    second = password[password_rule.max_count - 1]
+    return (first == password_rule.letter) != (second == password_rule.letter)
+
+
+def part_2(password_rules_list: List[str]) -> int:
+    valid_passwords = 0
+    for item in password_rules_list:
+        rules = parse_password_rules(item)
+        password = item.split()[2]
+        valid_passwords += 1 if validate_password_new(rules, password) else 0
     return valid_passwords
 
 
 if __name__ == "__main__":
     password_list = open("day2.txt").read().split("\n")
-    print(part1(password_list))
+    print(part_1(password_list))
+    print(part_2(password_list))
