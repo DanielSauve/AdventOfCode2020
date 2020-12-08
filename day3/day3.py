d771acc57f332de48a155fc572aa1f0843e3a7d5
danielsauve <danielsauve@protonmail.com>
2020-12-07 21:48:42 -0800

    Solution to Day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -1,22 +1,36 @@
 from typing import List
+from functools import reduce
 
 
 def parse_trees(trees: str) -> List[str]:
     return trees.split("\n")
 
 
-def part_1(trees: str) -> int:
-    tree_grid = parse_trees(trees)
-    current_x = 3
+def traverse_trees(tree_grid: List[str], x_increment: int, y_increment: int) -> int:
     trees_encountered = 0
-    for current_y in range(1, len(tree_grid)):
+    current_x = x_increment
+    current_y = y_increment
+    tree_grid_width = len(tree_grid[0])
+    while current_y < len(tree_grid):
         if tree_grid[current_y][current_x] == "#":
             trees_encountered += 1
-        current_x = (current_x + 3) % len(tree_grid[current_y])
-        pass
+        current_x = (current_x + x_increment) % tree_grid_width
+        current_y += y_increment
     return trees_encountered
+
+
+def part_1(trees: str) -> int:
+    tree_grid = parse_trees(trees)
+    return traverse_trees(tree_grid, 3, 1)
+
+
+def part_2(trees: str) -> int:
+    tree_grid = parse_trees(trees)
+    return reduce(lambda x, y: x * y, [traverse_trees(tree_grid, i, 1) for i in [1, 3, 5, 7]]) * traverse_trees(
+        tree_grid, 1, 2)
 
 
 if __name__ == "__main__":
     trees_input = open("day3.txt").read()
     print(part_1(trees_input))
+    print(part_2(trees_input))
