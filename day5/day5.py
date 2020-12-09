13cce5e70f9624f75f24256c45b02826d60617fa
danielsauve <danielsauve@protonmail.com>
2020-12-08 20:51:03 -0800

    Solution to Day 5 Part 2

--- day5/day5.py
+++ day5/day5.py
@@ -18,12 +18,27 @@ def find_column(code: str) -> int:
     return low if code[-1] == "L" else high
 
 
+def compute_id(boarding_pass: str) -> int:
+    return find_row(boarding_pass[:-3]) * 8 + find_column(boarding_pass[-3:])
+
+
 def part_1(batch_file: str) -> int:
     boarding_passes = batch_file.split()
-    return max(
-        map(lambda boarding_pass: find_row(boarding_pass[:-3]) * 8 + find_column(boarding_pass[-3:]), boarding_passes))
+    return max(map(compute_id, boarding_passes))
+
+
+def part_2(batch_file: str) -> int:
+    boarding_passes = batch_file.split()
+    seat_ids = sorted((map(compute_id, boarding_passes)))
+    prev_id = seat_ids[0]
+    for seat_id in seat_ids[1:]:
+        if prev_id != seat_id - 1:
+            return seat_id - 1
+        prev_id = seat_id
+    return -1
 
 
 if __name__ == "__main__":
     boarding_input = open("day5.txt").read()
     print(part_1(boarding_input))
+    print(part_2(boarding_input))
