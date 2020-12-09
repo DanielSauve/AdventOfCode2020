1a74041b1d6ea584fdc9c0b1c92c231b4063e20a
danielsauve <danielsauve@protonmail.com>
2020-12-08 22:38:31 -0800

    Solution to Day 7 Part 2

--- day7/day7.py
+++ day7/day7.py
@@ -12,6 +12,17 @@ def parse_contains(contains: str) -> List[str]:
     return output
 
 
+def parse_contains_2(contains: str) -> Dict[str, int]:
+    if contains == "no other bags.":
+        return {}
+    output = {}
+    contains = contains[:-1]
+    for item in contains.split(", "):
+        bags_index = item.index(" bag")
+        output[item[2:bags_index]] = int(item[0])
+    return output
+
+
 def can_contain(bag_map: Dict[str, List[str]], start_bag: str, end_bag: str, memo: Dict[str, bool]) -> bool:
     if start_bag in memo:
         return memo[start_bag]
@@ -41,6 +52,26 @@ def part_1(bag_rules: str) -> int:
     return bag_count
 
 
+def count_bags_in(bag_map: Dict[str, Dict[str, int]], start_bag: str) -> int:
+    sub_bags = bag_map[start_bag]
+    if len(sub_bags) == 0:
+        return 1
+    bag_count = 1
+    for bag, count in sub_bags.items():
+        bag_count += count * count_bags_in(bag_map, bag)
+    return bag_count
+
+
+def part_2(bag_rules: str) -> int:
+    bag_rules_list = bag_rules.split("\n")
+    bag_map: Dict[str, Dict[str, int]] = dict()
+    for rule in bag_rules_list:
+        bag, contains = rule.split(" bags contain ")
+        bag_map[bag] = parse_contains_2(contains)
+    return count_bags_in(bag_map, "shiny gold") - 1
+
+
 if __name__ == "__main__":
     raw_bag_rules = open("day7.txt").read()
     print(part_1(raw_bag_rules))
+    print(part_2(raw_bag_rules))
