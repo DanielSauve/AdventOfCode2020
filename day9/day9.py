19f0b27c7f5907e1d1fd0594b6b4a18366326307
danielsauve <danielsauve@protonmail.com>
2020-12-10 12:56:15 -0800

    Solution for Day 9 Part 2
    Brute force again.

--- day9/day9.py
+++ day9/day9.py
@@ -19,6 +19,17 @@ def part_1(raw_xmas: str) -> int:
     return 0
 
 
+def part_2(raw_xmas: str, invalid_number: int) -> int:
+    xmas_list = list(map(lambda x: int(x), raw_xmas.split()))
+    for i in range(len(xmas_list)):
+        for j in range(i + 1, len(xmas_list)):
+            if sum(xmas_list[i:j]) == invalid_number:
+                return min(xmas_list[i:j+1]) + max(xmas_list[i:j+1])
+    return -1
+
+
 if __name__ == "__main__":
     xmas = open("day9.txt").read()
-    print(part_1(xmas))
+    vulnerable = part_1(xmas)
+    print(vulnerable)
+    print(part_2(xmas, vulnerable))
